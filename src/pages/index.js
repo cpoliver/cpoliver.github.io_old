876e04db42d837fd139fe383900396945313eeff
Charles Oliver <charles.oliver@casumo.com>
2018-10-07 12:23:08 +0200

    start new gatsby project

---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import Layout from "../components/layout";
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hello, World!</h1>
+    <p>Content to go here&hellip;</p>
+  </Layout>
+);
+
+export default IndexPage;
