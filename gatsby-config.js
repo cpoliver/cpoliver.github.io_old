3117c080900103a4f28e7967ec8909b494871eb8
Charles Oliver <charles.oliver@casumo.com>
2018-10-07 15:15:42 +0200

    add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,21 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: "Charles P. Oliver | Full-Stack Developer"
   },
   plugins: [
-    'gatsby-plugin-react-helmet',
+    "gatsby-plugin-favicon",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-react-helmet",
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#663399',
-        theme_color: '#663399',
-        display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    'gatsby-plugin-offline',
-  ],
-}
+        name: "cpoliver.com",
+        short_name: "starter",
+        start_url: "/",
+        background_color: "#663399",
+        theme_color: "#663399",
+        display: "minimal-ui",
+        icon: "src/favicon.png" // This path is relative to the root of the site.
+      }
+    }
+  ]
+};
